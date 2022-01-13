845d626121109e6074f32a65ce56e5bf3778739f
gmulhearn <immpumpz@gmail.com>
2022-01-13 12:23:57 +1000

    autofill and fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Box, Paper, TextField, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import Button from '@mui/material/Button';
-import { useState } from 'react';
-import { signTxAndBroadcast } from './StakeHelper';
+import { useEffect, useState } from 'react';
+import { constructTx, signTxAndBroadcast } from './StakeHelper';
+import { config } from './config';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,30 +18,6 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-const mainnet_anonyome_tx = { // updatedTx
-  "msg": {
-    "typeUrl": "/cosmos.staking.v1beta1.MsgDelegate",
-    "value": {
-      "delegatorAddress": "cheqd1wtnqqzs2pk05vvttc3cy6pq5shmx9fqnaxdrq2",
-      "validatorAddress": "cheqdvaloper1djh8syeug9ylw7v8ca6jvq3s59klzmm4fm949w",
-      "amount": {
-        "amount": "10000000000",
-        "denom": "ncheq"
-      }
-    }
-  },
-  "fee": {
-    "amount": [
-      {
-        "amount": "10000000",
-        "denom": "ncheq"
-      }
-    ],
-    "gas": "200000"
-  },
-  "memo": ""
-}
-
 function App() {
   const classes = useStyles()
 
@@ -48,8 +25,21 @@ function App() {
   const [delegatorAddr, setDelegatorAddr] = useState("")
   const [validatorAddr, setValidatorAddr] = useState("")
 
+  useEffect(async() => {
+    await window.keplr.enable(config.CHAIN_ID);
+    
+        const offlineSigner = window.keplr.getOfflineSigner(config.CHAIN_ID);
+        const accounts = await offlineSigner.getAccounts();
+
+        if (accounts[0]) {
+          console.log(accounts)
+          setDelegatorAddr(accounts[0].address)
+        }
+  }, [])
+
   const stakeClick = () => {
-    signTxAndBroadcast(mainnet_anonyome_tx, "cheqd1wtnqqzs2pk05vvttc3cy6pq5shmx9fqnaxdrq2", () => { })
+    const tx = constructTx(delegatorAddr, validatorAddr, stakeAmount)
+    signTxAndBroadcast(tx, delegatorAddr, () => { })
   }
 
   return (
@@ -58,7 +48,7 @@ function App() {
         <Paper elevator={3} className={classes.paper}>
           <Typography variant="h4" margin={"1em"} textAlign={"center"}>Keplr Staking</Typography>
           <TextField fullWidth placeholder='Stake amount (CHEQ)...' onChange={(e) => setStakeAmount(e.target.value)}></TextField>
-          <TextField fullWidth placeholder='Your Keplr address (cheqd1wt...)' onChange={(e) => setDelegatorAddr(e.target.value)}></TextField>
+          <TextField fullWidth value={delegatorAddr} placeholder='Your Keplr address (cheqd1wt...)' onChange={(e) => setDelegatorAddr(e.target.value)}></TextField>
           <TextField fullWidth placeholder='Validator address (cheqdvaloper1d...)' onChange={(e) => setValidatorAddr(e.target.value)}></TextField>
           <Button variant="contained" fullWidth style={{ marginTop: "2em" }} onClick={stakeClick}>Stake</Button>
         </Paper>
